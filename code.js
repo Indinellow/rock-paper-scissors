1440fb09c5465e20713a835744b0992ffd6e46d7
Bojan <bojan.ostic@gmail.com>
2021-12-22 22:19:37 +0100

    code more or less finalized, need to upgrade css next

--- code.js
+++ code.js
@@ -5,6 +5,15 @@ function getRandomIntFromInterval(min, max) { // min and max included
 function playComputer() {
     let possibleChoices = ['rock','paper','scissors'];
     let pick = getRandomIntFromInterval(0,2);
+    const computerRock = document.querySelector('.computerRock');
+    const computerScissors = document.querySelector('.computerScissors');
+    const computerPaper = document.querySelector('.computerPaper');
+    
+    if(pick===0){computerRock.classList.add('chosen');}
+    else if(pick===1){computerPaper.classList.add('chosen');}
+    else{computerScissors.classList.add('chosen');}
+    
+    
     return possibleChoices[pick];
 } 
 
@@ -57,27 +66,7 @@ function decideWhoWins(playerSelection,computerSelection){
     }
 }
 
-
-
-    let playerScore = 0;
-    let computerScore = 0;
-    const scoreContainer = document.querySelector('.scoreContainer');
-    const currentScore = document.createElement('div');
-    currentScore.textContent=`Current score: ${playerScore} - ${computerScore}`;
-    scoreContainer.appendChild(currentScore);
-
-
-function playOneRound(playerChoice){
-    let computerChoice = playComputer();
-    let whoWon = decideWhoWins(playerChoice,computerChoice);
-    if (whoWon[1] === 3){
-        playerScore++;
-    }
-    else if (whoWon[1] === 2){
-        computerScore++;
-    }
-    currentScore.textContent=`Current score: ${playerScore} - ${computerScore}`;
-
+function checkScore(){
     if(playerScore>= 5){
         currentScore.textContent=`You Won! Final Score: ${playerScore} - ${computerScore}`
         rockButton.removeEventListener('click',playOneRoundRock);
@@ -94,45 +83,81 @@ function playOneRound(playerChoice){
 
 }
 
+function initializeGame(){
+    playerScore=0;
+    computerScore=0;
+    currentScore.textContent=`Current score: ${playerScore} - ${computerScore}`;
+    rockButton.addEventListener('click', playOneRoundRock);
+    paperButton.addEventListener('click', playOneRoundPaper);
+    scissorsButton.addEventListener('click', playOneRoundScissors );
+
+}
+
+function playOneRound(playerChoice){
+    let computerChoice = playComputer();
+    let whoWon = decideWhoWins(playerChoice,computerChoice);
+    if (whoWon[1] === 3){
+        playerScore++;
+    }
+    else if (whoWon[1] === 2){
+        computerScore++;
+    }
+    currentScore.textContent=`Current score: ${playerScore} - ${computerScore}`;
+    checkScore();
+
+}
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('chosen');
+  }
+
 function playOneRoundRock(){
     playOneRound('rock');
+    const rockButton = document.querySelector('#rockButton');
+    rockButton.classList.add('chosen'); 
 }
 
 function playOneRoundScissors(){
     playOneRound('scissors');
+    const scissorsButton = document.querySelector('#scissorsButton');
+    scissorsButton.classList.add('chosen');
 }
 
 function playOneRoundPaper(){
     playOneRound('paper');
+    const paperButton = document.querySelector('#paperButton');
+    paperButton.classList.add('chosen');
 }
+const restartButton = document.querySelector('#restart');
+restartButton.addEventListener('click',initializeGame);
+
+let playerScore = 0;
+let computerScore = 0;
+const scoreContainer = document.querySelector('.scoreContainer');
+const currentScore = document.createElement('div');
+currentScore.textContent=`Current score: ${playerScore} - ${computerScore}`;
+scoreContainer.appendChild(currentScore);
 
 const buttons = document.querySelectorAll('button');
 
 const rockButton = document.querySelector('#rockButton');
     rockButton.addEventListener('click', playOneRoundRock);
+    rockButton.addEventListener('transitionend',removeTransition); 
 
 const paperButton = document.querySelector('#paperButton');
     paperButton.addEventListener('click', playOneRoundPaper);
+    paperButton.addEventListener('transitionend',removeTransition); 
 
 const scissorsButton = document.querySelector('#scissorsButton');
     scissorsButton.addEventListener('click', playOneRoundScissors );
+    scissorsButton.addEventListener('transitionend',removeTransition); 
 
-/*
-window.addEventListener('click',()=>{
+const computerRock = document.querySelector('.computerRock');
+const computerScissors = document.querySelector('.computerScissors');
+const computerPaper = document.querySelector('.computerPaper');
 
+computerPaper.addEventListener('transitionend',removeTransition); 
+computerRock.addEventListener('transitionend',removeTransition); 
+computerScissors.addEventListener('transitionend',removeTransition); 
 
-    if(playerScore>=5){
-        currentScore.textContent=`You Won! Final score: ${playerScore} - ${computerScore}!`;
-        rockButton.removeEventListener('click',() => {
-            let playerChoice='rock';
-            playOneRound(playerChoice);
-        });
-    }
-    if(computerScore>=5){
-        currentScore.textContent=`You lost! Final score: ${playerScore} - ${computerScore}!`;
-
-    }
-
-})
-
-*/
+    
